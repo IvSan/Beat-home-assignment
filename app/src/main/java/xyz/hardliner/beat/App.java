bc9ba1876141f97a61fa5d69402420dc6f564968
IvSan <foritun@gmail.com>
2021-02-06 18:34:21 +0200

    command line handling

--- app/src/main/java/xyz/hardliner/beat/App.java
+++ app/src/main/java/xyz/hardliner/beat/App.java
@@ -1,5 +1,10 @@
 package xyz.hardliner.beat;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import xyz.hardliner.beat.service.DataProcessor;
@@ -11,15 +16,48 @@ public class App {
 
     private static final Logger log = LoggerFactory.getLogger(App.class);
 
-    private static final String input = "./app/src/test/resources/time_sorted_paths.csv";
-    private static final String output = "./app/src/test/resources/result.csv";
+    public static void main(String[] args) throws ParseException {
+        var cmd = commandLine(args);
+        var input = cmd.getOptionValue("i");
+        var output = cmd.getOptionValue("o");
+        validateOptions(input, output);
 
-    public static void main(String[] args) {
-        var unit = new DataProcessor(
+        var dataProcessor = new DataProcessor(
             new FileReader(input),
             new RidesHandler(),
             new ResultFileWriter(output));
 
-        unit.process();
+        try {
+            dataProcessor.process();
+        } catch (Exception ex) {
+            if (log.isDebugEnabled()) {
+                log.error("Cannot process data: " + ex.getMessage(), ex);
+            } else {
+                log.error("Cannot process data: " + ex.getMessage());
+            }
+        }
+    }
+
+    private static CommandLine commandLine(String[] args) throws ParseException {
+        CommandLineParser parser = new DefaultParser();
+        return parser.parse(commandLineOptions(), args);
+    }
+
+    private static Options commandLineOptions() {
+        Options options = new Options();
+        options.addOption("i", "input", true, "Path of input file");
+        options.addOption("o", "output", true, "Path for out file");
+        return options;
+    }
+
+    private static void validateOptions(String input, String output) {
+        if (input == null || input.isBlank()) {
+            log.error("Please, specify correct input file path.");
+            System.exit(1);
+        }
+        if (output == null || output.isBlank()) {
+            log.error("Please, specify correct output file path.");
+            System.exit(1);
+        }
     }
 }
